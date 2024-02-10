3b66904e76a7f303a735119e708d8c9155acfba4
Luis Rivera <luisi.najera@gmail.com>
2024-02-10 00:24:56 -0600

    check

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
+app.get('/', function (req, res) {
+  
+      res.end( "Funciona" );
+})
+
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
